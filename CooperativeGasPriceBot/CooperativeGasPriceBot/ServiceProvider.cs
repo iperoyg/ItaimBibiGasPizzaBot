89da64c52d0f5ee00e94340384c908ef503cdc42
Arthur Iperoyg <iperoyg@gmail.com>
2017-09-11 22:12:23 -0300

    implements basic service provider and add it to application.json serviceprovidertype

--- CooperativeGasPriceBot/CooperativeGasPriceBot/ServiceProvider.cs
+++ CooperativeGasPriceBot/CooperativeGasPriceBot/ServiceProvider.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using SimpleInjector;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Takenet.MessagingHub.Client.Host;
 
 namespace CooperativeGasPriceBot
 {
-    public class ServiceProvider
+    public class ServiceProvider : Container, IServiceContainer
     {
+
+        public ServiceProvider()
+        {
+
+        }
+
+        public void RegisterService(Type serviceType, object instance)
+        {
+            RegisterSingleton(serviceType, instance);
+        }
+
+        public void RegisterService(Type serviceType, Func<object> instanceFactory)
+        {
+            RegisterSingleton(serviceType, instanceFactory);
+        }
     }
 }
