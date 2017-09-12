acb950deb133fee79191485742e67a7b564f0229
Arthur Iperoyg <iperoyg@gmail.com>
2017-09-12 00:29:09 -0300

    add location receiver and set application.json to filter location media type to location receiver

---
+++ CooperativeGasPriceBot/CooperativeGasPriceBot/Receivers/LocationMessageReceiver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Lime.Protocol;
+using Takenet.MessagingHub.Client.Listener;
+using Lime.Messaging.Contents;
+using Takenet.MessagingHub.Client.Sender;
+using Takenet.MessagingHub.Client;
+
+namespace CooperativeGasPriceBot.Receivers
+{
+    public class LocationMessageReceiver : IMessageReceiver
+    {
+        private readonly IMessagingHubSender _sender;
+
+        public LocationMessageReceiver(
+            IMessagingHubSender sender
+            )
+        {
+            _sender = sender;
+        }
+
+        public async Task ReceiveAsync(Message envelope, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var location = envelope.Content as Location;
+            await _sender.SendMessageAsync($"Localização recebida!\nLat: {location.Latitude};\nLon: {location.Longitude}", envelope.From, cancellationToken);
+        }
+    }
+}
