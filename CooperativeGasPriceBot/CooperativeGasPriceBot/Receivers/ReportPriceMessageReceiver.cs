376242afef43eafb6405984989fe25b117513761
Arthur Iperoyg <iperoyg@gmail.com>
2017-09-12 00:07:20 -0300

    add receivers to report and search price messages; change message receivers priority

---
+++ CooperativeGasPriceBot/CooperativeGasPriceBot/Receivers/ReportPriceMessageReceiver.cs
@@ -0,0 +1,43 @@
+﻿using Lime.Messaging.Contents;
+using Lime.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Takenet.MessagingHub.Client;
+using Takenet.MessagingHub.Client.Listener;
+using Takenet.MessagingHub.Client.Sender;
+
+namespace CooperativeGasPriceBot.Receivers
+{
+    public class ReportPriceMessageReceiver : IMessageReceiver
+    {
+        private readonly IMessagingHubSender _sender;
+
+        public ReportPriceMessageReceiver(
+            IMessagingHubSender sender
+            )
+        {
+            _sender = sender;
+        }
+
+        public async Task ReceiveAsync(Message envelope, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var input = new Input
+            {
+                Validation = new InputValidation
+                {
+                    Rule = InputValidationRule.Type,
+                    Type = Location.MediaType,
+                },
+                Label = new DocumentContainer
+                {
+                    Value = PlainText.Parse("Certo... Para reportar um preço próximo de vc, informe sua localização.")
+                }
+            };
+            await _sender.SendMessageAsync(input, envelope.From, cancellationToken);
+        }
+    }
+}
---
+++ CooperativeGasPriceBot/CooperativeGasPriceBot/Receivers/SearchPriceMessageReceiver.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Lime.Protocol;
+using Takenet.MessagingHub.Client.Listener;
+using Lime.Messaging.Contents;
+using Takenet.MessagingHub.Client.Sender;
+using Takenet.MessagingHub.Client;
+
+namespace CooperativeGasPriceBot.Receivers
+{
+    public class SearchPriceMessageReceiver : IMessageReceiver
+    {
+        private readonly IMessagingHubSender _sender;
+
+        public SearchPriceMessageReceiver(
+            IMessagingHubSender sender
+            )
+        {
+            _sender = sender;
+        }
+
+        public async  Task ReceiveAsync(Message envelope, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var input = new Input
+            {
+                Validation = new InputValidation
+                {
+                    Rule = InputValidationRule.Type,
+                    Type = Location.MediaType,
+                },
+                Label = new DocumentContainer
+                {
+                    Value = PlainText.Parse("Ótimo!\nPara informar os preços próximos à vc, primeiro preciso da sua localização.")
+                }
+            };
+            await _sender.SendMessageAsync(input, envelope.From, cancellationToken);
+        }
+    }
+}
